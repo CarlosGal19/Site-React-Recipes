b9eaae74edd54b23337b636954d10d7d9a3c1150
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-16 13:59:31 -0600

    style: change color of header and footer in white mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
 
     return (
         <nav
-            className="relative flex w-full flex-nowrap items-center justify-between bg-zinc-50 py-2 text-neutral-500 shadow-dark-mild hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4"
+            className="relative flex w-full flex-nowrap items-center justify-between bg-teal-500 py-2 text-neutral-500 shadow-dark-mild hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4"
             data-twe-navbar-ref>
             <div className="flex w-full flex-wrap items-center justify-between px-3">
                 <div className="ms-2">
@@ -80,7 +80,7 @@ const Header: React.FC = () => {
                                     checked={darkMode}
                                     className="sr-only peer"
                                 />
-                                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                <div className="relative w-11 h-6 bg-gray-800 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-500"></div>
                                 <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
                             </label>
                         </li>
--- src/components/static/Footer.tsx
+++ src/components/static/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <>
             <footer
-                className="bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
+                className="bg-teal-500 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
                 <div
                     className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
                     <div className="me-12 hidden lg:block">
