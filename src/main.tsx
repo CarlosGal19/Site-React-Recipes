2c8026cdbdd1540f59927b9deea8960fff6332a2
Carlos Galindo <utm22030587@utma.edu.mx>
2024-06-17 14:47:12 -0600

    feat: create your-recipes route

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import Favorites from './components/Favorites.tsx'
 import Header from './components/Header.tsx'
 import Footer from './components/static/Footer.tsx'
 import NoMatch from './components/static/NoMatch.tsx'
+import YourRecipes from './components/YourRecipes.tsx'
 import { FavoriteProvider } from './context/FavoriteProvider.tsx'
 import { DarkModeProvider } from './context/DarkModeProvider.tsx'
 import './index.css'
@@ -23,8 +24,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path='/' element={<App />} />
           <Route path='/recipes' element={<Recipes />} />
           <Route path='/meals' element={<Meals />}/>
-            <Route path='/recipe' element={<Recipe />} />
-            <Route path='/favorites' element={<Favorites/>}/>
+          <Route path='/recipe' element={<Recipe />} />
+          <Route path='/favorites' element={<Favorites/>}/>
+          <Route path='/your-recipes' element={<YourRecipes/>}/>
           <Route path='*' element={<NoMatch />} />
       </Routes>
     </BrowserRouter>
